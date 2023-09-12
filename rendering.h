a66150c028285a9c1bf239267253bbeb9172b167
Joel Allen <joelallen720@gmail.com>
2023-09-12 15:19:05 -0400

    Switch to rendering on single primary command buffer for debugging.

--- rendering.h
+++ rendering.h
@@ -29,28 +29,59 @@ static VkResult NextFrame()
 
 static VkCommandBuffer BeginRenderCommands(RenderTarget* render_target, uint32 render_thread_index)
 {
+CTK_UNUSED(render_thread_index)
     Frame* frame = GetCurrentPtr(&global_ctx.frames);
-    VkCommandBuffer command_buffer = Get(&frame->render_command_buffers, render_thread_index);
+    // VkCommandBuffer command_buffer = Get(&frame->render_command_buffers, render_thread_index);
 
-    VkCommandBufferInheritanceInfo inheritance_info =
+    // VkCommandBufferInheritanceInfo inheritance_info =
+    // {
+    //     .sType                = VK_STRUCTURE_TYPE_COMMAND_BUFFER_INHERITANCE_INFO,
+    //     .pNext                = NULL,
+    //     .renderPass           = render_target->render_pass,
+    //     .subpass              = 0,
+    //     .framebuffer          = Get(&render_target->framebuffers, frame->swapchain_image_index),
+    //     .occlusionQueryEnable = VK_FALSE,
+    //     .queryFlags           = 0,
+    //     .pipelineStatistics   = 0,
+    // };
+    // VkCommandBufferBeginInfo begin_info =
+    // {
+    //     .sType            = VK_STRUCTURE_TYPE_COMMAND_BUFFER_BEGIN_INFO,
+    //     .flags            = VK_COMMAND_BUFFER_USAGE_RENDER_PASS_CONTINUE_BIT,
+    //     .pInheritanceInfo = &inheritance_info,
+    // };
+    // VkResult res = vkBeginCommandBuffer(command_buffer, &begin_info);
+    // Validate(res, "vkBeginCommandBuffer() failed");
+
+// Record current frame's primary command buffer, including render command buffers.
+VkCommandBuffer command_buffer = frame->primary_render_command_buffer;
+
+// Begin command buffer.
+VkCommandBufferBeginInfo command_buffer_begin_info =
+{
+    .sType            = VK_STRUCTURE_TYPE_COMMAND_BUFFER_BEGIN_INFO,
+    .flags            = 0,
+    .pInheritanceInfo = NULL,
+};
+VkResult res = vkBeginCommandBuffer(command_buffer, &command_buffer_begin_info);
+Validate(res, "vkBeginCommandBuffer() failed");
+
+// Begin render pass.
+VkRenderPassBeginInfo render_pass_begin_info =
+{
+    .sType       = VK_STRUCTURE_TYPE_RENDER_PASS_BEGIN_INFO,
+    .pNext       = NULL,
+    .renderPass  = render_target->render_pass,
+    .framebuffer = Get(&render_target->framebuffers, frame->swapchain_image_index),
+    .renderArea  =
     {
-        .sType                = VK_STRUCTURE_TYPE_COMMAND_BUFFER_INHERITANCE_INFO,
-        .pNext                = NULL,
-        .renderPass           = render_target->render_pass,
-        .subpass              = 0,
-        .framebuffer          = Get(&render_target->framebuffers, frame->swapchain_image_index),
-        .occlusionQueryEnable = VK_FALSE,
-        .queryFlags           = 0,
-        .pipelineStatistics   = 0,
-    };
-    VkCommandBufferBeginInfo begin_info =
-    {
-        .sType            = VK_STRUCTURE_TYPE_COMMAND_BUFFER_BEGIN_INFO,
-        .flags            = VK_COMMAND_BUFFER_USAGE_RENDER_PASS_CONTINUE_BIT,
-        .pInheritanceInfo = &inheritance_info,
-    };
-    VkResult res = vkBeginCommandBuffer(command_buffer, &begin_info);
-    Validate(res, "vkBeginCommandBuffer() failed");
+        .offset = { 0, 0 },
+        .extent = render_target->extent,
+    },
+    .clearValueCount = render_target->attachment_clear_values.count,
+    .pClearValues    = render_target->attachment_clear_values.data,
+};
+vkCmdBeginRenderPass(command_buffer, &render_pass_begin_info, VK_SUBPASS_CONTENTS_INLINE);
 
     return command_buffer;
 }
@@ -100,52 +131,54 @@ static void DrawMesh(VkCommandBuffer command_buffer, Mesh* mesh, uint32 instance
 
 static void EndRenderCommands(VkCommandBuffer command_buffer)
 {
+vkCmdEndRenderPass(command_buffer);
     VkResult res = vkEndCommandBuffer(command_buffer);
     Validate(res, "vkEndCommandBuffer() failed");
 }
 
 static void SubmitRenderCommands(RenderTarget* render_target)
 {
+CTK_UNUSED(render_target)
     Frame* frame = GetCurrentPtr(&global_ctx.frames);
     VkResult res = VK_SUCCESS;
 
     // Record current frame's primary command buffer, including render command buffers.
     VkCommandBuffer command_buffer = frame->primary_render_command_buffer;
 
-    // Begin command buffer.
-    VkCommandBufferBeginInfo command_buffer_begin_info =
-    {
-        .sType            = VK_STRUCTURE_TYPE_COMMAND_BUFFER_BEGIN_INFO,
-        .flags            = 0,
-        .pInheritanceInfo = NULL,
-    };
-    res = vkBeginCommandBuffer(command_buffer, &command_buffer_begin_info);
-    Validate(res, "vkBeginCommandBuffer() failed");
+    // // Begin command buffer.
+    // VkCommandBufferBeginInfo command_buffer_begin_info =
+    // {
+    //     .sType            = VK_STRUCTURE_TYPE_COMMAND_BUFFER_BEGIN_INFO,
+    //     .flags            = 0,
+    //     .pInheritanceInfo = NULL,
+    // };
+    // res = vkBeginCommandBuffer(command_buffer, &command_buffer_begin_info);
+    // Validate(res, "vkBeginCommandBuffer() failed");
 
-    // Begin render pass.
-    VkRenderPassBeginInfo render_pass_begin_info =
-    {
-        .sType       = VK_STRUCTURE_TYPE_RENDER_PASS_BEGIN_INFO,
-        .pNext       = NULL,
-        .renderPass  = render_target->render_pass,
-        .framebuffer = Get(&render_target->framebuffers, frame->swapchain_image_index),
-        .renderArea  =
-        {
-            .offset = { 0, 0 },
-            .extent = render_target->extent,
-        },
-        .clearValueCount = render_target->attachment_clear_values.count,
-        .pClearValues    = render_target->attachment_clear_values.data,
-    };
-    vkCmdBeginRenderPass(command_buffer, &render_pass_begin_info, VK_SUBPASS_CONTENTS_SECONDARY_COMMAND_BUFFERS);
+    // // Begin render pass.
+    // VkRenderPassBeginInfo render_pass_begin_info =
+    // {
+    //     .sType       = VK_STRUCTURE_TYPE_RENDER_PASS_BEGIN_INFO,
+    //     .pNext       = NULL,
+    //     .renderPass  = render_target->render_pass,
+    //     .framebuffer = Get(&render_target->framebuffers, frame->swapchain_image_index),
+    //     .renderArea  =
+    //     {
+    //         .offset = { 0, 0 },
+    //         .extent = render_target->extent,
+    //     },
+    //     .clearValueCount = render_target->attachment_clear_values.count,
+    //     .pClearValues    = render_target->attachment_clear_values.data,
+    // };
+    // vkCmdBeginRenderPass(command_buffer, &render_pass_begin_info, VK_SUBPASS_CONTENTS_SECONDARY_COMMAND_BUFFERS);
 
-    // Execute entity render commands.
-    vkCmdExecuteCommands(command_buffer, frame->render_command_buffers.count, frame->render_command_buffers.data);
+    // // Execute entity render commands.
+    // vkCmdExecuteCommands(command_buffer, frame->render_command_buffers.count, frame->render_command_buffers.data);
 
-    vkCmdEndRenderPass(command_buffer);
+    // vkCmdEndRenderPass(command_buffer);
 
-    res = vkEndCommandBuffer(command_buffer);
-    Validate(res, "vkEndCommandBuffer() failed");
+    // res = vkEndCommandBuffer(command_buffer);
+    // Validate(res, "vkEndCommandBuffer() failed");
 
     // Submit commands for rendering to graphics queue.
     VkPipelineStageFlags wait_stage = VK_PIPELINE_STAGE_COLOR_ATTACHMENT_OUTPUT_BIT;

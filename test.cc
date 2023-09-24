85cd01db783dac1a2dfef26ede4704a7263a51ca
Joel Allen <joelallen720@gmail.com>
2023-09-24 10:02:40 -0400

    Split all test files between original and new versions.

--- test.cc
+++ test.cc
@@ -1,6 +1,14 @@
+// #define USE_ORIGINAL
+
 #include "ctk3/ctk3.h"
-// #include "rtk/tests/3d/main.h"
+
+#ifdef USE_ORIGINAL
+#include "rtk/tests/3d/main.h"
 #include "rtk/tests/debug/main.h"
+#else
+#include "rtk/tests/3d/main_2.h"
+#include "rtk/tests/debug/main_2.h"
+#endif
 
 sint32 main()
 {

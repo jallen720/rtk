3e829d0653d29b7f59d48355d5d5c0d3ef7afba1
Joel Allen <joelallen720@gmail.com>
2023-11-24 17:14:45 -0500

    Update meshes to match blender vertex/index ordering.

--- meshes/quad.h
+++ meshes/quad.h
@@ -1,9 +1,10 @@
 Vertex quad_vertexes[] =
 {
+    // -Z
     { .position = { 0.0f, 0.0f, 0.0f }, .uv = { 0.50f, 1.00f } },
-    { .position = { 0.0f, 1.0f, 0.0f }, .uv = { 0.50f, 0.50f } },
-    { .position = { 1.0f, 1.0f, 0.0f }, .uv = { 0.75f, 0.50f } },
     { .position = { 1.0f, 0.0f, 0.0f }, .uv = { 0.75f, 1.00f } },
+    { .position = { 1.0f, 1.0f, 0.0f }, .uv = { 0.75f, 0.50f } },
+    { .position = { 0.0f, 1.0f, 0.0f }, .uv = { 0.50f, 0.50f } },
 };
 
 uint32 quad_indexes[] =

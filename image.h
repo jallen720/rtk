d882ea2cec10358679427a552fb165dd99c936e0
Joel Allen <joelallen720@gmail.com>
2024-01-20 10:30:41 -0500

    Add MipLevels() for calculating mip-levels based on largest dimension of ImageData.

--- image.h
+++ image.h
@@ -44,6 +44,11 @@ static void LoadImageData(ImageData* image_data, const char* path)
     image_data->size = image_data->width * image_data->height * image_data->channel_count;
 }
 
+static uint32 MipLevels(ImageData* image_data)
+{
+    return ((uint32)Log2((float32)Max(image_data->width, image_data->height))) + 1;
+}
+
 static void DestroyImageData(ImageData* image_data)
 {
     stbi_image_free(image_data->data);

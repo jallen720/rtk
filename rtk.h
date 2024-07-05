e1ee3d9ab9e8b5778392dd386fb7eb1c897fcf0e
Joel Allen <joelallen720@gmail.com>
2024-07-05 12:31:51 -0400

    Cleanup.

--- rtk.h
+++ rtk.h
@@ -16,27 +16,29 @@ using namespace CTK;
 
 namespace RTK
 {
-    // Utils
-    #include "rtk/debug.h"
-    #include "rtk/vk_array.h"
-    #include "rtk/device_features.h"
 
-    // Context
-    #include "rtk/context.h"
+// Utils
+#include "rtk/debug.h"
+#include "rtk/vk_array.h"
+#include "rtk/device_features.h"
 
-    // Resources
-    #include "rtk/resource.h"
-    #include "rtk/buffer.h"
-    #include "rtk/image.h"
+// Context
+#include "rtk/context.h"
 
-    // Assets
-    #include "rtk/gltf.h"
-    #include "rtk/mesh.h"
-    #include "rtk/descriptor_set.h"
-    #include "rtk/render_target.h"
-    #include "rtk/pipeline.h"
+// Resources
+#include "rtk/resource.h"
+#include "rtk/buffer.h"
+#include "rtk/image.h"
 
-    // Misc.
-    #include "rtk/rendering.h"
-    #include "rtk/frame_metrics.h"
+// Assets
+#include "rtk/gltf.h"
+#include "rtk/mesh.h"
+#include "rtk/descriptor_set.h"
+#include "rtk/render_target.h"
+#include "rtk/pipeline.h"
+
+// Misc.
+#include "rtk/rendering.h"
+#include "rtk/frame_metrics.h"
+
 }
